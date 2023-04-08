3eee908630dc5b718951f9aef0747cdf1517fa0c
miamilemon <kontakt.thexenonz@gmail.com>
2023-04-08 23:57:08 +0200

    zaczynamy powazne programowanie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+var poziom = "";
+
 function Menu(){
+    if(localStorage.getItem("poziom")==null){
+        poziom = "1";
+    }else{
+        poziom = localStorage.getItem("poziom");
+    }
     return(
         <div className="flex flex-col justify-center items-center bg-teal-800 h-[100vh]">
             <div className=" relative flex flex-col rounded-[20px] max-w-[300px] bg-teal-900 bg-clip-border shadow-3xl shadow-shadow-xl flex flex-col w-full !p-6 3xl:p-![18px] undefined">
@@ -9,7 +16,7 @@ function Menu(){
                   <h4 className="pb-8 self-center drop-shadow-xl text-xl font-bold text-white">
                       Sokoban
                   </h4>
-                  <button className="rounded-xl bg-gradient-to-b from-teal-300 to-teal-500 px-5 py-3 text-base mb-3 font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-teal-700">
+                  <button onClick={Gra(poziom)} className="rounded-xl bg-gradient-to-b from-teal-300 to-teal-500 px-5 py-3 text-base mb-3 font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-teal-700">
                       Graj
                   </button>
                   <button className="rounded-xl bg-gradient-to-b from-teal-500 to-teal-700 px-5 py-3 text-base mb-3 font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-teal-800">
@@ -21,6 +28,12 @@ function Menu(){
             Made by: <a className="hover:text-gray-200" href="github.com/miamilemon">Nikodem M</a>
             </h6>
         </div>
+    );
+}
+
+function Gra() {
+    return (
+        console.log(poziom+"testest")
     );
 }
 
