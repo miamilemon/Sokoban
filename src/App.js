10c36ac8f554fd3b8b86012396288280ac66fed8
miamilemon <kontakt.thexenonz@gmail.com>
2023-04-09 17:59:22 +0200

    poczatek designu okna gry

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import * as ReactDOMClient from 'react-dom/client';
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
-//Mapy poszczegolnych poziomow gry
+//Mapy poszczegolnych poziomow gry w postaci tablicy 3d
 //Jaki numer odpowiada jakiemu elementowi mapy
 //0=podloga, 1=sciana, 2=blok, 3=gracz, 4=pole do ustawienia bloku
 const Mapa = [
-    [
+    [//Mapa poziomu 1
         [1, 1, 1, 1, 1, 1, 1, 1],
         [1, 4, 1, 1, 0, 0, 0, 1],
         [1, 0, 1, 0, 0, 0, 1, 1],
@@ -32,7 +32,9 @@ const Mapa = [
     ]
 ]
 
-var poziom = "";
+function RenderMapy(ktora){
+    //const stanMapy = Mapa[ktora].map
+}
 
 function Menu(){
     //renderujemy
@@ -141,13 +143,27 @@ function WybierzPoziom(){
 }
 
 function Gra(poziom) {
-    return (
-        console.log(poziom+"testest")
+    root.render(
+        <div className="flex flex-col justify-center items-center bg-teal-800 h-[100vh]">
+            <div className="flex flex-row items-start text-left justify-start max-w-[450px] w-full">
+                <button onClick={()=>Menu()} className="self-start mr-4 rounded-xl bg-gradient-to-b from-teal-300 to-teal-500 px-3 py-3 text-base font-medium text-white transition duration-200 hover:shadow-lg hover:shadow-teal-700">
+                      Powrót
+                </button>
+                <div className="float-left">
+                    <h1 className="drop-shadow-xl text-md font-bold text-white">Czas: </h1>
+                    <h1 className="pb-2 drop-shadow-xl text-md font-bold text-white"> Ilość kroków: </h1>
+                </div>
+            </div>
+            <div className=" relative flex flex-col rounded-[20px] max-w-[500px] max-h-[500px] bg-teal-900 bg-clip-border shadow-3xl shadow-shadow-xl flex flex-col h-full w-full !p-6 3xl:p-![18px] undefined">
+              
+            </div>
+        </div>
     );
 }
 
 function App() {
-    Menu();
+    //Menu();
+    Gra(1);
 }
 
 export default App;
